ef6bc78ec7dee26d801c5da7eff14f6dfa84036d
FredoSalat <fredrik.marthinsens@gmail.com>
2023-11-09 20:11:58 +0100

    importing local font, swedish sans (regular, semibold and bold) to be used as classes in tailwind (text-regular/bold/semibold) with regular as default

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
+import localFont from "@next/font/local";
+
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
@@ -8,7 +10,25 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 
-const inter = Inter({ subsets: ["latin"] });
+const swedenSans = localFont({
+  src: [
+    {
+      path: "/../public/fonts/SwedenSansRegular.woff2",
+      weight: "400",
+    },
+    {
+      path: "/../public/fonts/SwedenSansBold.woff2",
+      weight: "700",
+    },
+    {
+      path: "/../public/fonts/SwedenSansSemiBold.woff2",
+      weight: "600",
+    },
+  ],
+  variable: "--font-SwedenSans",
+});
+
+//const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Fredrik | Personal Portfolio",
@@ -23,7 +43,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={`${swedenSans.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.65rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        swedenSans: ["var(--font-SwedenSans"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
