ba58bf87024b8518d9bc70d0c07db7839b74b961
FredoSalat <fredrik.marthinsens@gmail.com>
2023-11-17 19:42:32 +0100

    fixed button and small fix to height of first section

--- components/intro.tsx
+++ components/intro.tsx
@@ -18,7 +18,7 @@ export default function Intro() {
     <section
       ref={ref}
       id="home"
-      className="flex flex-col sm:h-screen lg:min-h-[1000px] md:min-h-[1100px] sm:min-h-[1200px] sm:max-w-[70rem] scroll-mt-[100rem] items-center justify-center sm:mb-0 mb-10 gap-2"
+      className="flex flex-col sm:h-screen lg:min-h-[900px] md:min-h-[1100px] sm:min-h-[1200px] sm:max-w-[70rem] scroll-mt-[100rem] items-center justify-center sm:mb-0 mb-10 gap-2"
     >
       <motion.div
         initial={{ opacity: 0, y: 100 }}
@@ -32,19 +32,19 @@ export default function Intro() {
 
         <p className="font-regular text-2xl sm:text-3xl !leading-[1.5] sm:!leading-[1.4] mb-10 px-4">
           Earlier in my career, I founded a scale-up company in the AI and
-          Medtech industry and worked as a user researcher for companies such as
+          MedTech industry and worked as a user researcher for companies such as
           Spotify.
         </p>
       </motion.div>
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className="flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-regular"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.1 }}
       >
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none  hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
@@ -55,7 +55,7 @@ export default function Intro() {
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:hover:text-white"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:hover:text-white"
           href="/CV.pdf"
           download
         >
@@ -64,7 +64,7 @@ export default function Intro() {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 hover:text-[#0077B5] flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 dark:hover:text-[#0077B5] "
+          className="bg-white p-4 text-gray-700 hover:text-[#0077B5] flex items-center gap-2 rounded-full  hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 dark:hover:text-[#0077B5] "
           href="https://www.linkedin.com/in/fredrik-marthinsen/"
           target="_blank"
         >
@@ -72,7 +72,7 @@ export default function Intro() {
         </a>
 
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.30rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-[#171515] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 dark:hover:text-white"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.286rem] rounded-full hover:scale-[1.15] hover:text-[#171515] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 dark:hover:text-white"
           href="https://github.com/FredoSalat"
           target="_blank"
         >
